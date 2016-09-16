82ab17dfa6c31fef102fd42b45548c7ee6f910a7
Stephen2Page <stephen2page@gmail.com>
2016-09-16 15:32:04 -0400

    files prepaired, no code

---
+++ VirtualPet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        //Contains program interface
+        static void Main(string[] args)
+        {
+            //Display Health
+
+            //Display Activities
+
+            //Request input
+
+            //Evaluate input
+
+        }
+    }
+}
